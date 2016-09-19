d8592c4a0173b0309401bb1b5658a754fe31930a
fabiofalavinha <14[fafgh23tuxzumb37]*>
2016-09-19 19:51:18 -0300

    setup project

---
+++ src/main/java/com/zbra/go/ZbraGoApplication.java
@@ -0,0 +1,12 @@
+package com.zbra.go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbraGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZbraGoApplication.class, args);
+	}
+}
