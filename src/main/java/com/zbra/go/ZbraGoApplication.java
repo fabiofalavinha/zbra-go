675b7e234a667a69f690d81c984bc59ebdd2c55f
Caio Maia <caaiomaia@gmail.com>
2016-09-29 02:47:18 -0300

    Configurations and simplification of springboot application class

--- src/main/java/com/zbra/go/ZbraGoApplication.java
+++ src/main/java/com/zbra/go/ZbraGoApplication.java
@@ -1,12 +1,16 @@
 package com.zbra.go;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@ComponentScan("com.zbra.go")
+@EnableAutoConfiguration
 public class ZbraGoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZbraGoApplication.class, args);
 	}
+
 }
